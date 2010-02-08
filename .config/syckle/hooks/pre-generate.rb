4df244778efd4f474b6648ab15253a2f4ba5b182
7rans <transfire@gmail.com>
2010-02-08 18:26:51 -0500

    major: add phase hooks

---
+++ .config/syckle/hooks/pre-generate.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+file  = "lib/#{project.name}.rb"
+text1 = read(file)
+text2 = text1.sub(/VERSION\s*\=\s*\S+/, %{VERSION = "#{project.metadata.version}"})
+if text1 != text2
+  write(file, text2)
+  report "Updated #{file}"
+else
+  report "Already current #{file}"
+end
+
