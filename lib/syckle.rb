817503e403374560257e1f8a6b57b09a32edaeb6
7rans <transfire@gmail.com>
2010-06-10 08:48:37 -0400

    add metadata access

--- lib/syckle.rb
+++ lib/syckle.rb
@@ -1,4 +1,21 @@
 module Syckle
-  VERSION = "0.9.1"
+  DIRECTORY = File.dirname(__FILE__) + '/syckle'
+
+  PROFILE = YAML.load(File.new(DIRECTORY + '/profile.yml'))
+  verfile = YAML.load(File.new(DIRECTORY + '/version.yml'))
+
+  VERSION = verfile.values_at('major','minor','patch','state','build').compact.join('.')
+
+  #
+  def self.const_missing(name)
+    key = name.to_s.downcase
+    if verfile.key?(key)
+      verfile[key]
+    elsif profile.key?(key)
+      PROFILE[key]
+    else
+      super(name)
+    end
+  end
 end
 
