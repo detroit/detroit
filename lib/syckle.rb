9b10a157fd2da4cab1fec5853c26c8c0d2fbd7c7
7rans <transfire@gmail.com>
2010-12-17 14:54:26 -0500

    admin: update metadata per latest pom

--- lib/syckle.rb
+++ lib/syckle.rb
@@ -1,17 +1,20 @@
 require 'yaml'
 
 module Syckle
-  DIRECTORY = File.dirname(__FILE__) + '/syckle'
-
-  PROFILE = YAML.load(File.new(DIRECTORY + '/profile.yml'))
-  VERFILE = YAML.load(File.new(DIRECTORY + '/version.yml'))
-
-  VERSION = VERFILE.values_at(*%w{major minor patch build}).join('.')
-
-  #
-  def self.const_missing(name)
-    key = name.to_s.downcase
-    VERFILE[key] || PROFILE[key] || super(name)
+  # Access to project metadata.
+  def self.metadata
+    @metadata ||= (
+      require 'yaml'
+      YAML.load(File.new(File.dirname(__FILE__) + '/syckle.yml'))
+    )
   end
+
+  # Access to project metadata via constants.
+  def self.const_missing(name)
+    metadata[name.to_s.downcase] || super(name)
+  end
+
+  # TODO: Only here b/c of bug in Ruby 1.8.x
+  VERSION = metadata['version']
 end
 
