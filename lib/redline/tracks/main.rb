3bd695ac03e823e3609be6424454045e47b97060
7rans <transfire@gmail.com>
2011-05-31 23:26:56 -0400

    add install and deploy stops to main track

--- lib/redline/tracks/main.rb
+++ lib/redline/tracks/main.rb
@@ -1,24 +1,28 @@
 module Redline
 
+  # TODO: Should :install comes before :verify b/c verfication might
+  # require a local installation?
+
   # Main track.
   track :main do
 
     route :prepare,    # prepare services / ensure service requirements
           :generate,   # code generation
           :compile,    # compile source code
-          :test,       # run tests and/or specifications
-          :analyze,    # run code analysis
+          :test,       # run tests and specifications
+          :analyze,    # perform code analysis
           :document,   # generate documentation
           :package,    # create packages
-          :verify,     # post package verification
+          :verify,     # post package verification / integration tests
+          :install,    # install the package locally (if need be)
           :publish,    # publish documentation/website
           :release,    # release packages
-          :deploy,     # deploy to servers
+          :deploy,     # deploy system to servers
           :announce    # tell the world about your awesome work
 
-    route :reset,      # mark artifacts as out-of-date
-          :clean,      # remove temporary artifacts
-          :purge       # remove all generate files
+    route :reset,      # mark products as out-of-date
+          :clean,      # remove temporary products
+          :purge       # remove all generated products
 
   end
 
